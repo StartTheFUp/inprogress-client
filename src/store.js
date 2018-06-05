004e90fcba1492d090e8dbb4437cfa7d78d541d3
bogdanceu <ceubogdan@yahoo.com>
2018-06-05 09:49:38 +0200

    create store et fetch les blocs

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
 import { createStore } from 'redux'
+import {actions} from './action/index.js'
+import {reducer} from './reducers/index.js'
 
 const initialState = {
   blocs: []
 }
+
+export const store = createStore(reducer, initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+fetch('/blocs')
+  .then(res => res.json())
+  .then(blocs => actions.loadBlocs(blocs))
