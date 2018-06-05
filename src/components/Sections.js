7972e12824c972470348e0c3cdf200cd8af5ab38
bogdanceu <ceubogdan@yahoo.com>
2018-06-05 11:59:15 +0200

    create component Section

---
+++ src/components/Sections.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Elements from './Elements.js'
+
+const Sections = (section) => {
+  return (
+    <div key={section._id}>
+      <h2>{section.title}</h2>
+      <div>{section.elements.map(Elements)}</div>
+
+    </div>
+  )
+}
+
+export default Sections
