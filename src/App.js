b374333d38a886ede980b3aa35332dd3c25a7b6e
SophieMdl <sophie.michaudel@gmail.com>
2018-06-05 11:52:46 +0200

    COmposant header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
 import 'semantic-ui-css/semantic.min.css'
 
+import HeaderDashboard from './components/HeaderDashboard'
+
 class App extends Component {
   render () {
     return (
       <div className="App">
+        <HeaderDashboard />
       </div>
     )
   }
