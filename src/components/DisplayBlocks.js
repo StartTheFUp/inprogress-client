19a41735cbb209dbd435edec2792e2e568cda8f8
bogdanceu <ceubogdan@yahoo.com>
2018-06-06 11:55:15 +0200

    create one function typeBlocks

--- src/components/DisplayBlocks.js
+++ src/components/DisplayBlocks.js
@@ -2,24 +2,28 @@ import React from 'react'
 import './DisplayBlocks.css'
 import 'semantic-ui-css/semantic.min.css'
 import Blocks from './Blocks.js'
-const todoBlocks = (blocks) => blocks.filter(block => block.type === 'todos')
-const billetBlocks = (blocks) => blocks.filter(block => block.type === 'billets')
-const ressourceBlocks = (blocks) => blocks.filter(block => block.type === 'ressources')
+
+// const todoBlocks = (blocks) => blocks.filter(block => block.type === 'todos')
+// const billetBlocks = (blocks) => blocks.filter(block => block.type === 'billets')
+// const ressourceBlocks = (blocks) => blocks.filter(block => block.type === 'ressources')
+
+const typeBlocks = (blocks, Type) => blocks.filter(block => block.type === Type)
+
 const DisplayBlocks = (props) => {
   return (
     <div className='Blocks'>
 
       <div className='otherBlocks'>
         <div className='Billets'>
-          {billetBlocks(props.blocks).map(Blocks)}
+          {typeBlocks(props.blocks, 'billets').map(Blocks)}
         </div>
         <div className='Ressources'>
-          {ressourceBlocks(props.blocks).map(Blocks)}
+          {typeBlocks(props.blocks, 'ressources').map(Blocks)}
         </div>
       </div>
 
       <div className='todos'>
-        {todoBlocks(props.blocks).map(Blocks)}
+        {typeBlocks(props.blocks, 'todos').map(Blocks)}
       </div>
     </div>
   )
