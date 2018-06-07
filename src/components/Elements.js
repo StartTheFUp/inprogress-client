7b1bf6be99572f0b33cf323e12776ac6d7df3a04
gaelleF <gaelle.fougere@outlook.fr>
2018-06-07 12:11:59 +0200

    ajout checkbox si type elements = todos

--- src/components/Elements.js
+++ src/components/Elements.js
@@ -3,9 +3,15 @@ import React from 'react'
 const Elements = elem => {
   return (
     <div key={elem.createdAt} className="allElem">
-      <p>{elem.content}</p>
+    <div className="ui checkbox" style={{display : (elem.type === "todos"?"block":"none") }}>
+      <input className="example" type="checkbox"/>
+      <label>  <p>{elem.content}</p>   </label>
+    </div>
+    <p style={{display : (elem.type === "todos"?"none":"block") }}>{elem.content}</p> 
     </div>
   )
 }
 
 export default Elements
+
+//<input name="checkBoxName"  checktype="checkbox" onClick={() => console.log(this)}>
