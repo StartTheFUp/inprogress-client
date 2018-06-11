e84bb788d952566f2ea1545f67efedcc8870e911
gaelleF <gaelle.fougere@outlook.fr>
2018-06-11 21:03:21 +0200

    action todos non fonctionnel !

--- src/containers/Block.js
+++ src/containers/Block.js
@@ -2,9 +2,10 @@ import React from 'react'
 import DisplayBlock from '../components/DisplayBlock.js'
 import Section from './Section.js'
 const Block = ({block, idBlock}) =>
-  <DisplayBlock block={block}>
+
+ <DisplayBlock block={block}>
     {block.sections.map((section) => {
-      console.log("idSection", section.id)
+      console.log("idSection", idBlock)
       return <Section key={section.id} section={section} idBlock={idBlock} idSection={section.id}/>
     })}
   </DisplayBlock>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,10 +6,11 @@ export const reducer = (state, action) => {
     }
   }
   if (action.type === 'UPDATE_TODOS') {
-    //const newBlock = state.blocks
-    console.log("state block", state)
+    //const newBlock = state.blocks   state.blocks.filter(block=>block._id === action.idBlock.filter(block => block.sections.id === action.idSection).filter(block=>block.sections.element.id === action.idElement).filter(block=>block.sections.elements.id === action.idElement)
+    console.log("state block",state.blocks.filter(block=>block._id === action.idBlock.idBlock).filter(block => block.sections.id === action.idSection))
     // console.log("action updateTodo", action.indexBlock, action.indexSection, action.indexElement, newBlock[action.indexBlock].sections[action.indexSection].elements[action.indexElement].properties.checked)
-    //console.log('action updateTodo', action.idBlock, action.idSection, action.idElement, newBlock[1].sections[0].elements[1].properties.checked)
+    console.log('action updateTodo',state.blocks.map(block=> block._id), action.idBlock, action.idSection, action.idElement)
+    console.log("action idBlock", action.idBlock)
     // newBlock[action.indexBlock].sections[action.indexSection].elements[action.indexElement].properties.checked = !(newBlock[action.indexBlock].sections[action.indexSection].elements[action.indexElement].properties.checked)
     //newBlock[1].sections[0].elements[1].properties.checked = !(newBlock[1].sections[0].elements[1].properties.checked)
     return {
