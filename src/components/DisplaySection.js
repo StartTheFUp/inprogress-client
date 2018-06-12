c21150096ec8beaec0453f13314306be89c96cd7
bogdanceu <ceubogdan@yahoo.com>
2018-06-12 13:08:24 +0200

    show only unprocessed tickets

--- src/components/DisplaySection.js
+++ src/components/DisplaySection.js
@@ -1,9 +1,9 @@
 import React from 'react'
-const DisplaySection = ({section, idBlock, idSection, children}) => {
+const DisplaySection = ({section, idBlock, idSection, children}) => {console.log('childrensection', {children})
   return (
     <div key={section.title}>
       <h2>{section.title}</h2>
-      <div>{children}</div>
+      <div>{children.filter(({props}) => props.elem.properties.archive === false)}</div>
     </div>
   )
 }
