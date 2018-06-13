0a03d14fe93115c892efebac688029e1b5a52c05
Axel Martin <martin.axe@live.fr>
2018-06-13 18:08:59 +0200

    add dumb new billet

--- src/components/DisplaySection.js
+++ src/components/DisplaySection.js
@@ -1,17 +1,18 @@
 import React from 'react'
-const DisplaySection = ({section, children, blockType, processedTickets}) => {
+const DisplaySection = ({ addNewBillet, idSection, section, children, blockType, processedTickets }) => {
   if (processedTickets === true && blockType === 'billets') {
     return (
       <div key={section.title}>
         <h2>{section.title}</h2>
-        <div>{children.filter(({props}) => props.elem.properties.archive === true)}</div>
+        <div>{children.filter(({ props }) => props.elem.properties.archive === true)}</div>
       </div>
     )
   } else if (processedTickets === false && blockType === 'billets') {
     return (
       <div key={section.title}>
+        <button onClick={() => addNewBillet({ idSection })}> New Billet</button>
         <h2>{section.title}</h2>
-        <div>{children.filter(({props}) => props.elem.properties.archive === false)}</div>
+        <div>{children.filter(({ props }) => props.elem.properties.archive === false)}</div>
       </div>
     )
   } else {
