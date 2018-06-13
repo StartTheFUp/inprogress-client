cf206bdd28ca3ab1702b86246e45768ae044b940
bogdanceu <ceubogdan@yahoo.com>
2018-06-13 12:07:03 +0200

    fetch moved into componentDidMount and create function dispatch

--- src/containers/Block.js
+++ src/containers/Block.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import DisplayBlock from '../components/DisplayBlock.js'
 import Section from './Section.js'
-import {actions} from '../actions/file.js'
+import {showUnprocessedTickets, showProcessedTickets} from '../actions/file.js'
 
 const Block = ({block, idBlock, processedTickets}) =>
 
-  <DisplayBlock block={block} showUnprocessedTickets={actions.showUnprocessedTickets} showProcessedTickets={actions.showProcessedTickets} processedTickets={processedTickets}>
+  <DisplayBlock block={block} showUnprocessedTickets={showUnprocessedTickets} showProcessedTickets={showProcessedTickets} processedTickets={processedTickets}>
     {block.sections.map((section) => {
       console.log('idSection', idBlock)
       return <Section key={section.id} section={section} idBlock={idBlock} idSection={section.id} processedTickets={processedTickets} blockType={block.type}/>
--- src/containers/Element.js
+++ src/containers/Element.js
@@ -1,8 +1,8 @@
-import { actions } from '../actions/file.js'
+import { updateTodo } from '../actions/file.js'
 import React from 'react'
 import DisplayElement from '../components/DisplayElement.js'
 
 const Element = (props) =>
-  <DisplayElement {...props} elementAction={actions.updateTodo} />
+  <DisplayElement {...props} elementAction={updateTodo} />
 
 export default Element
