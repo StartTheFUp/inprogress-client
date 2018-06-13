b17df5802334ed54e712c8f832cb7631201670c9
bogdanceu <ceubogdan@yahoo.com>
2018-06-13 09:56:09 +0200

    regler bug titre billets en cours

--- src/components/DisplayBlock.js
+++ src/components/DisplayBlock.js
@@ -5,7 +5,7 @@ import './../style/DisplayBlock.css'
 const DisplayBlock = ({ block, children, processedTickets, showProcessedTickets, showUnprocessedTickets }) => {
   return (
     <Segment key={block._id}>
-      <h1 className={(processedTickets === false ? 'unprocessed-ticket' : 'processed-ticket')} onClick={() => showUnprocessedTickets()}>{block.title}</h1>
+      <h1 className={(block.type === 'billets' ? (processedTickets === false ? 'unprocessed-ticket' : 'processed-ticket') : '')} onClick={() => showUnprocessedTickets()}>{block.title}</h1>
       <div className={(processedTickets === false ? 'processed-ticket' : 'unprocessed-ticket')} onClick={() => showProcessedTickets()} style={{ display: (block.type === 'billets' ? 'block' : 'none') }}>Billets traité</div>
       <Divider section />
       <div>{children}</div>
