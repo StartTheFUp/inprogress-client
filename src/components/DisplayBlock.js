7e2539c48248be5e877f6b523c360e47bb9cc6fb
SophieMdl <sophie.michaudel@gmail.com>
2018-06-13 09:40:54 +0200

    css class

--- src/components/DisplayBlock.js
+++ src/components/DisplayBlock.js
@@ -2,17 +2,14 @@ import React from 'react'
 import { Segment, Divider } from 'semantic-ui-react'
 import './../style/DisplayBlock.css'
 
-const DisplayBlock = ({block, children, processedTickets, showProcessedTickets, showUnprocessedTickets}) => {
+const DisplayBlock = ({ block, children, processedTickets, showProcessedTickets, showUnprocessedTickets }) => {
   return (
-    <div key={block._id} className="AllBlocks">
-      <Segment>
-        <h1 className={(processedTickets === false ? 'unprocessed-ticket' : 'processed-ticket')} onClick={() => showUnprocessedTickets()}>{block.title}</h1>
-        <div className={(processedTickets === false ? 'processed-ticket' : 'unprocessed-ticket')} onClick={() => showProcessedTickets()} style={{display: (block.type === 'billets' ? 'block' : 'none')}}>Billets traité</div>
-        <Divider section />
-        <div>{children}</div>
-      </Segment>
-
-    </div>
+    <Segment key={block._id}>
+      <h1 className={(processedTickets === false ? 'unprocessed-ticket' : 'processed-ticket')} onClick={() => showUnprocessedTickets()}>{block.title}</h1>
+      <div className={(processedTickets === false ? 'processed-ticket' : 'unprocessed-ticket')} onClick={() => showProcessedTickets()} style={{ display: (block.type === 'billets' ? 'block' : 'none') }}>Billets traité</div>
+      <Divider section />
+      <div>{children}</div>
+    </Segment>
   )
 }
 
