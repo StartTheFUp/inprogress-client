d6ef2fd70768732d210b61005bbb24aea61d7935
Yoann Cribier <yoann_cribier@hotmail.com>
2018-06-18 16:24:08 +0200

    api - smart wrapper to switch between 'development' (mocks) & 'production' -- Use `serve mocks` on port 5000 (port specified by your proxy) to start your mocked API

--- src/api.js
+++ src/api.js
@@ -1,11 +1,14 @@
-const getBlocks = () => fetch('/blocks')
+const path = route => process.env.NODE_ENV === "development" ? route + '.json' : route
+
+const getBlocks = () => fetch(path('/blocks'))
   .then(res => res.json())
 
-const getComments = () => fetch('/comments')
+const getComments = () => fetch(path('/comments'))
   .then(res => res.json())
 
-const getProjectById = id => fetch(`/project/${id}`)
+const getProjectById = id => fetch(path(`/projects/${id}`))
   .then(res => res.json())
+
 export default {
   getBlocks,
   getComments,
