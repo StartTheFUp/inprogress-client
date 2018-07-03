bb2ccf3d37a0dd95692a530f96ec6115aa2f836f
bogdanceu <ceubogdan@yahoo.com>
2018-07-03 12:58:26 +0200

    drag and drop resource

--- src/components/ResourceSection.js
+++ src/components/ResourceSection.js
@@ -1,31 +1,18 @@
 import ResourceElement from './ResourceElement'
 import React from 'react'
-import { addNewElement } from '../actions/file.js'
+import { addNewElement, dragDropElements } from '../actions/file.js'
 import { store } from '../store.js'
 import Dragula from 'react-dragula'
 import '../style/ResourceSection.css'
 
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list)
-  const [removed] = result.splice(startIndex, 1)
-  result.splice(endIndex, 0, removed)
-
-  return result
-}
- const moveItem = (array, fromIndex, toIndex) =>{
-  const arrayCopy = array.slice()
-  const item = arrayCopy[fromIndex]
-  arrayCopy.splice(fromIndex, 1) // remove field that's moving
-  arrayCopy.splice(toIndex, 0, item) // add it back
-  return arrayCopy
-}
 class ResourceSection extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      items: this.props.section.elements
+      items: this.props.section.elements,
+      start: 0,
+      end: 0
     }
-
   }
   componentDidMount () {
     this.unsubscribe = store.subscribe(() => this.forceUpdate())
@@ -35,11 +22,40 @@ class ResourceSection extends React.Component {
     this.unsubscribe()
   }
 
+   moveItem = (array, fromIndex, toIndex) => {
+     const arrayCopy = array.slice()
+     const item = arrayCopy[fromIndex]
+     arrayCopy.splice(fromIndex, 1) // remove field that's moving
+     arrayCopy.splice(toIndex, 0, item) // add it back
+     return arrayCopy
+   }
+
+  getIndexInParent = (el) => {
+    const i = Array.from(el.parentNode.children).indexOf(el)
+    console.log({i})
+    return i
+  }
+
   dragulaDecorator = (componentBackingInstance) => {
-    if (componentBackingInstance) {
-      let options = {}
-      Dragula([componentBackingInstance], options)
-    }
+    // if (componentBackingInstance) {
+    //   let options = {}
+    // }
+    const drake = Dragula([componentBackingInstance]) //, options)
+
+    drake.on('drag', (el, target, source, sibling) => {
+      console.log('drag')
+      let newStart = this.getIndexInParent(el)
+      this.setState({items: this.props.section.elements, start: newStart})
+      console.log('nouveau items', this.state.items)
+    })
+
+    drake.on('drop', (el, target, source, sibling) => {
+      console.log('drop')
+      let droppedLocation = this.getIndexInParent(el)
+      let result = this.moveItem(this.state.items, this.state.start, droppedLocation)
+      this.setState({items: result})
+      dragDropElements(this.state.items, this.props.block._id, this.props.section.id)
+    })
   }
 
   getElements = () => this.props.section.elements
@@ -49,9 +65,14 @@ class ResourceSection extends React.Component {
       blockId={this.props.block._id}
       sectionId={this.props.section.id}
       activeElement={this.props.activeElement}
+
     />)
 
- render () {
+
+ // getIndexInParent = (el) => Array.from(el.parentNode.children).indexOf(el)
+
+
+  render () {
     console.log('noulnoul', this.state.items)
     return (
       <div key={this.props.section.id}>
