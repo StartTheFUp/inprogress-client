b910e736a562a262d4a0c69e6a3e5087aeb9792b
gaelleF <gaelle.fougere@outlook.fr>
2018-07-17 14:25:18 +0200

    focus on add Section input

--- src/components/ButtonAddSection.js
+++ src/components/ButtonAddSection.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../style/ButtonAddSection.css'
 import { Input, Button } from 'semantic-ui-react'
 
-const ButtonAddSection = ({ blockId, showAddSection, addSection, addSectionActive }) => {
-  let title = ''
+class ButtonAddSection extends Component {
+  focusInput () {
+    this.refs.textInput.focus()
+  }
 
-  return (
-    <div className="addSection">
-      <div className="show-form-add-section" onClick={() => showAddSection(blockId)} style={{display: (addSectionActive !== blockId ? 'inline-block' : 'none')}}>
-          + Ajouter une section
+  render () {
+    let title = ''
+    return (
+      <div className="addSection">
+        <div className="show-form-add-section" onClick={() => this.props.showAddSection(this.props.blockId) } style={{display: (this.props.addSectionActive !== this.props.blockId ? 'inline-block' : 'none')}}>
+              + Ajouter une section
+        </div>
+        <div className="add-section-form" style={{display: (this.props.addSectionActive === this.props.blockId ? 'inline-block' : 'none')}}>
+          <Input className="add-title-focus" placeholder="titre" name="title" onChange={(event) => (title = event.target.value)} ref='textInput' onPointerOver={() => this.focusInput()} />
+          <Button.Group className="button-group-section">
+            <Button className="button-add-section" value = "Ajouter la section" onClick={() => this.props.addSection(this.props.blockId, title)}>Ajouter</Button>
+            <Button.Or />
+            <Button className="close-section-form" onClick = {() => this.props.showAddSection(this.props.blockId) }>Annuler </Button>
+          </Button.Group>
+        </div>
       </div>
-      <div className="add-section-form" style={{display: (addSectionActive === blockId ? 'inline-block' : 'none')}}>
-        <Input className="add-title-form" placeholder="titre" name="title" onChange={(event) => (title = event.target.value)} />
-        <Button.Group className="button-group-section">
-          <Button className="button-add-section" value = "Ajouter la section" onClick={() => addSection(blockId, title)}>Ajouter</Button>
-          <Button.Or />
-          <Button className="close-section-form" onClick = {() => showAddSection(blockId) }>Annuler </Button>
-        </Button.Group>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ButtonAddSection
