9518d3e213e1991ada72e24156ccc4b5bd1b4d1c
mcoenca <m.coenca@gmail.com>
2018-08-30 16:56:07 +0200

    adding api config infos

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const { hostname } = window.location
 
 export const apiHost = hostname === '127.0.0.1' || hostname === 'localhost'
   ? 'http://localhost:5000'
-  : 'https://wcs-inprogress-api.herokuapp.com'
+  : process.env.REACT_APP_API_URL
 
 const path = route => `${apiHost}${route}` + (process.env.REACT_APP_MOCKS ? '.json' : '')
 
